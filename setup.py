d0ffa1fa2aa239df2e5df4ec2aa88062cf5a10f5
Mark Watts <wattsmark2015@gmail.com>
2015-07-17 13:33:45 -0500

    Changed FuXi dependency to "production" version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 feature_deps = {}
 
 for feature_file in glob("*.requirements.txt"):
-    feature,_ = feature_file.split(".", 1)
+    feature, _ = feature_file.split(".", 1)
 
     with open(feature_file) as f:
         required = f.read().splitlines()
@@ -25,24 +25,24 @@ import os
 long_description = open("README.rst").read()
 
 setup(
-    name = "YAROM",
+    name="YAROM",
     install_requires=required,
     dependency_links=[
         "git://github.com/NeuralEnsemble/libNeuroML.git#egg=libNeuroML",
         "git://github.com/zopefoundation/ZODB.git#egg=ZODB",
         "git://github.com/RDFLib/rdflib-zodb.git#egg=ZODB",
-        ],
-    version = '0.6.3',
-    packages = ['yarom'],
-    package_data = {"yarom":['default.conf', 'rules.n3']},
-    author = "Mark Watts",
-    author_email = "wattsmark2015@gmail.com",
-    description = "Yet Another RDF-Object Mapper",
-    long_description = long_description,
-    license = "BSD 3-clause",
+    ],
+    version='0.6.3',
+    packages=['yarom'],
+    package_data={"yarom": ['default.conf', 'rules.n3']},
+    author="Mark Watts",
+    author_email="wattsmark2015@gmail.com",
+    description="Yet Another RDF-Object Mapper",
+    long_description=long_description,
+    license="BSD 3-clause",
     url="http://yarom.readthedocs.org/en/latest/",
-    download_url = 'https://github.com/mwatts15/YAROM/archive/master.zip',
-    classifiers = [
+    download_url='https://github.com/mwatts15/YAROM/archive/master.zip',
+    classifiers=[
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
@@ -52,5 +52,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering'],
     extras_require=feature_deps,
-    zip_safe = False
+    zip_safe=False
 )
