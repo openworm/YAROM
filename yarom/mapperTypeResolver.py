ac7c797753591e9f77d98eccf76e4261a8efb2d8
Mark Watts <wattsmark2015@gmail.com>
2016-03-13 14:16:45 -0500

    Removed explicit reference to DataObject in Resolver

--- yarom/mapperTypeResolver.py
+++ yarom/mapperTypeResolver.py
@@ -7,11 +7,10 @@ class Resolver(RDFTypeResolver):
 
     @classmethod
     def get_instance(cls):
-        from .dataObject import DataObject
         from .rdfUtils import deserialize_rdflib_term
         if cls.instance is None:
             cls.instance = RDFTypeResolver(
-                DataObject.rdf_type,
+                Mapper.get_instance().get_base_class().rdf_type,
                 Mapper.get_instance().get_most_specific_rdf_type,
                 Mapper.get_instance().oid,
                 deserialize_rdflib_term)
