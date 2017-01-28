da063b3cae4fecca6b531fbb3fb599274bf598d4
Mark Watts <watts.mark2015@gmail.com>
2017-01-27 20:32:17 -0600

    Removed python 3.2 build for setuptools compat

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Topic :: Scientific/Engineering'],
     extras_require=FEATURE_DEPS,
