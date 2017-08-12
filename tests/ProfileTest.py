694705e3d977fce7865ee3e0c7ed40b4b24a3d24
Mark Watts <watts.mark2015@gmail.com>
2017-08-12 09:31:56 -0500

    Adding a useful test to ProfileTest.py
    - Also, moving codespeed submit out of main Travis-CI build

---
+++ tests/ProfileTest.py
@@ -0,0 +1,18 @@
+"""
+Not actually tests, but benchmarks.
+"""
+from __future__ import print_function
+import yarom as Y
+from yarom import disconnect, connect
+
+
+def setup():
+    connect(configFile='tests/test_default.conf')
+
+
+def teardown():
+    disconnect()
+
+def test_do_create():
+    for i in range(10000):
+        Y.DataObject()
