1399b6e1c863688dd8fd2599556dcb1d1fa7c148
Mark Watts <watts.mark2015@gmail.com>
2017-08-12 14:33:42 -0500

    Adding checks for failed/errored jobs

--- .travis.sh
+++ .travis.sh
@@ -3,19 +3,15 @@
 pip install --upgrade 'setuptools' 'six>=1.9'
 pip install -r requirements.txt
 if [ $DEPLOY ] ; then
-    apt-get install ruby
+    sudo apt-get install ruby
     pip install twine
     gem install travis
 else
     if [ $INFERENCE ] ; then
-        echo pip install -r inference.requirements.txt
         pip install -r inference.requirements.txt
-
     fi
     if [ $ZODB ] ; then
-        echo pip install -r zodb.requirements.txt
         pip install -r zodb.requirements.txt
     fi
-
     python setup.py develop
 fi
--- travis-downstream-trigger.sh
+++ travis-downstream-trigger.sh
@@ -1,24 +1,46 @@
-#!/bin/bash -e
+#!/bin/bash -xe
+
+LAST_CACHE=0
+CACHE_INTERVAL=2
+CACHED_TRAVIS_JOBS=$(mktemp)
+
+get_cached_travis_jobs () {
+    NOW=$(date +"%s")
+    if [ $(( NOW - LAST_CACHE )) -ge $CACHE_INTERVAL ] ; then
+        travis show $TRAVIS_BUILD_NUMBER --no-interactive \
+            | grep "^#$TRAVIS_BUILD_NUMBER" \
+            | grep -v DEPLOY=1 > $CACHED_TRAVIS_JOBS
+        LAST_CACHE=$(date +"%s")
+    fi
+    cat $CACHED_TRAVIS_JOBS
+}
 
 get_passed_jobs () {
-    travis show $TRAVIS_BUILD_NUMBER --no-interactive \
-        | grep "^#$TRAVIS_BUILD_NUMBER" \
-        | grep -v DEPLOY=1 | grep passed | wc -l
+    get_cached_travis_jobs | grep passed | wc -l
+}
+
+get_failed_jobs () {
+    get_cached_travis_jobs | egrep 'fail|error|cancel' | wc -l
 }
 
 get_total_jobs () {
-    travis show $TRAVIS_BUILD_NUMBER --no-interactive \
-        | grep "^#$TRAVIS_BUILD_NUMBER" \
-        | grep -v DEPLOY=1 | wc -l
+    get_cached_travis_jobs | wc -l
 }
 
 if [ $DEPLOY ] ; then
     total_jobs=$(get_total_jobs)
     passed_jobs=$(get_passed_jobs)
-    while [ $total_jobs -ne $passed_jobs ] ; do
+    failed_jobs=$(get_failed_jobs)
+    while [ $failed_jobs -lt 1 -a $total_jobs -ne $passed_jobs ] ; do
         sleep 10
         passed_jobs=$(get_passed_jobs)
+        failed_jobs=$(get_failed_jobs)
     done
+
+    if [ $total_jobs -ne $passed_jobs ] ; then
+        echo "Build failed. Not triggering downstream builds"
+        exit
+    fi
 
     while read DOWNSTREAM_REPO DOWNSTREAM_BRANCH ; do
         echo "Attempting to trigger dowstream Travis-CI build of $DOWNSTREAM_REPO for branch $DOWNSTREAM_BRANCH"
