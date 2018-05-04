95f4ae8dfd1587f3d92ccd8b7dda08437699a7ab
Mark Watts <watts.mark2015@gmail.com>
2018-05-03 19:13:40 -0500

    Restricting deploys to the dev branch
    - Intension is for the latest snapshot to always be of the integrated
      version

--- .travis.success.sh
+++ .travis.success.sh
@@ -1,9 +1,12 @@
 #!/bin/sh -ex
 echo "$TRAVIS_COMMIT_MESSAGE" | head -n1 | grep -q '^MINOR:' && exit 0
 if [ $DEPLOY ] ; then
-    ./check-build-status.sh
-    ./deploy.sh
-    ./travis-downstream-trigger.sh
+    current_branch=$(git rev-parse --abbrev-ref HEAD)
+    if [ $current_branch == 'dev' ] ; then
+        ./check-build-status.sh
+        ./deploy.sh
+        ./travis-downstream-trigger.sh
+    fi
 else
     ./codespeed-submit.sh
 fi
