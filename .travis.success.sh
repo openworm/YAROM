8a7020e5744dc14a69543713d9ad238f97f3cb9c
Mark Watts <watts.mark2015@gmail.com>
2018-05-28 14:49:03 -0500

    Third times the charm

--- .travis.success.sh
+++ .travis.success.sh
@@ -2,8 +2,8 @@
 echo "$TRAVIS_COMMIT_MESSAGE" | head -n1 | grep -q '^MINOR:' && exit 0
 if [ $DEPLOY ] ; then
     HEAD_REV=$(git rev-parse HEAD)
-    ORIGIN_DEV_REV=$(git rev-parse origin/dev)
-    if [ "$HEAD_REV" != $ORIGIN_DEV_REV ] ; then
+    ORIGIN_DEV_REV=$(git ls-remote origin refs/heads/dev | grep -E -o '^[^[:space:]]+')
+    if [ "$HEAD_REV" != "$ORIGIN_DEV_REV" ] ; then
         echo "Not deploying since we aren't on the 'dev' branch" >&2
         exit 0
     fi
