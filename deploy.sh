e4425c68218661b1bd00e4064be2360a05a301a5
Mark Watts <watts.mark2015@gmail.com>
2018-09-16 16:42:32 -0500

    Modifying version number update code for deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
-python setup.py egg_info --tag-build=--post$(date +"%Y%m%d%H%M%S") sdist
+sed -i -r "s/__version__ = '([^']+).post0'/__version__ = '\\1.post$(date +"%Y%m%d%H%M%S")'/" yarom/__init__.py
+python setup.py egg_info sdist
 twine upload -c "Built by travis-ci. Uploaded after $(date +"%Y-%m-%d %H:%M:%S")" dist/YAROM*tar.gz
