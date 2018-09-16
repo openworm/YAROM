71857f70d95e4892e8e46fa680e0fde0a5c48408
Mark Watts <watts.mark2015@gmail.com>
2018-09-16 16:50:42 -0500

    Updating the version.txt as well

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
-sed -i -r "s/__version__ = '([^']+).post0'/__version__ = '\\1.post$(date +"%Y%m%d%H%M%S")'/" yarom/__init__.py
+date=$(date +"%Y%m%d%H%M%S")
+sed -i -r "s/__version__ = '([^']+)\\.post0'/__version__ = '\\1.post$date'/" yarom/__init__.py
+sed -i -r "s/^(.*)\\.post0/\\1.post$date/" version.txt
 python setup.py egg_info sdist
 twine upload -c "Built by travis-ci. Uploaded after $(date +"%Y-%m-%d %H:%M:%S")" dist/YAROM*tar.gz
