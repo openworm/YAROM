22dae924d0137f99b74d192215a9a2c4b665f429
Mark Watts <watts.mark2015@gmail.com>
2019-09-22 04:26:40 -0500

    Switching to dev releases from post-releases

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 date=$(date +"%Y%m%d%H%M%S")
-sed -i -r "s/__version__ = '([^']+)\\.post0'/__version__ = '\\1.post$date'/" yarom/__init__.py
-sed -i -r "s/^(.*)\\.post0/\\1.post$date/" version.txt
+sed -i -r "s/__version__ = '([^']+)\\.dev0'/__version__ = '\\1.dev$date'/" yarom/__init__.py
+sed -i -r "s/^(.*)\\.dev0/\\1.dev$date/" version.txt
 python setup.py egg_info sdist
 twine upload -c "Built by travis-ci. Uploaded after $(date +"%Y-%m-%d %H:%M:%S")" dist/YAROM*tar.gz
